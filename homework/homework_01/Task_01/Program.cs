226307e8e37d8b4019a6bc84693e71883bf5bfbb
YurijMelnikov <voolkan85@mail.ru>
2022-11-25 01:03:57 +0300

    Homework_01_task_01

--- homework/homework_01/Task_01/Program.cs
+++ homework/homework_01/Task_01/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает
+// два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Write ("Нахождение большего из двух рациональных чисел \nВведите первое число\n");
+double firstNumber = double.Parse (Console.ReadLine());
+Console.WriteLine ("Введите второе число");
+double secondNumber = double.Parse (Console.ReadLine());
+if (firstNumber == secondNumber)
+    {
+    Console.WriteLine ($"Ответ:\n{firstNumber} = {secondNumber}, числа равны");
+    }
+    if (firstNumber > secondNumber)
+    {
+        Console.WriteLine ($"Ответ:\n{firstNumber} больше {secondNumber}");
+    }
+        if (firstNumber < secondNumber)
+        {
+            Console.WriteLine ($"Ответ:\n{secondNumber} больше {firstNumber}");
+        }
