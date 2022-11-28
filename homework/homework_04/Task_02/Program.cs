a741fdb14fb104c2b570976bbc38777b26f792e8
YurijMelnikov <voolkan85@mail.ru>
2022-11-28 19:49:11 +0300

    Home Work 4

---
+++ homework/homework_04/Task_02/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+Console.WriteLine ("Введите целое число");
+int number = int.Parse(Console.ReadLine());
+int SumNumber(int integerAnyNumber)
+{
+    if (number > 0)
+    {
+        int sum = 0;
+        int num = number;
+        for (int n = 10; num  != 0; n = n * 10)
+        {    
+            sum = sum + (num % 10);
+            num = number / n;
+        }
+        return sum;
+    }
+    else
+    {
+        int sum = 0;
+        int num = number;
+        for (int n = 10; num  != 0; n = n * 10)
+        {    
+            sum = sum + (num % 10);
+            num = number / n;
+        }
+        return -sum;
+    }
+}
+
+int result = SumNumber(number);
+
+Console.WriteLine ($"Сумма цифр в числе {number} равна {result}");
---
+++ homework/homework_04/task_03/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 29: Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+ Console.WriteLine ("Инициализация массива целых чисел, состоящего из 8 элементов");
+int[] ArrayInitialization()
+{   
+    int[] array = new int [8];
+    for (int i = 0; i < 8; i++)
+    {
+        Console.WriteLine ($"Введите {i+1} элемент массива");
+        array[i] = Convert.ToInt32(Console.ReadLine());        
+    }
+    return array;
+}
+void PrintArray (int []arbitraryArray)
+{
+    Console.WriteLine ("Вывод введённого массива");
+    Console.Write ($"[{arbitraryArray[0]}, ");
+
+    for (int i = 1; i < arbitraryArray.Length-1; i++)
+    {
+        Console.Write ($"{arbitraryArray[i]}, ");
+    }
+    Console.Write ($"{arbitraryArray[7]}]");      
+}
+int [] array = ArrayInitialization();
+PrintArray (array);
